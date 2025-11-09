2264f566006457f4b4cbf83fbb23ae3a51c193c5
BryanBradfo <bryanchen1105@gmail.com>
2025-11-09 14:59:12 +0100

    add: last change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,12 @@ export default {
         'gray-700': '#21262d',
         'cyan-accent': '#00A8E8',
         'cyan-hover': '#007EA7',
+
+        // AJOUTEZ CES COULEURS
+        'light-bg': '#ffffff',      // Fond clair (blanc)
+        'light-text': '#0d1117',    // Texte clair (votre gris le plus foncé)
+        'dark-bg': '#0d1117',       // Fond sombre (votre gris le plus foncé)
+        'dark-text': '#f0f6fc',     // Texte sombre (un blanc cassé)
       },
     },
   },
